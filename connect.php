56e40e9ad656398e22f37e9da8bdcc7064ecd090
Robert Kaufman <rob.a.kaufman@gmail.com>
2019-11-02 13:48:38 -0500

    Added Bootstrap framework to main page

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
-    define('DB_USER','serveruser');
-    define('DB_PASS','gorgonzola7!');     
+    define('DB_DSN','mysql:host=localhost;dbname=bitcareers;charset=utf8');
+    define('DB_USER','Administrator');
+    define('DB_PASS','Password01');     
 
     // Create a PDO object called $db.
     $db = new PDO(DB_DSN, DB_USER, DB_PASS); 
